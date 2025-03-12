ace7c204be01e22a6b0179c4627e325f32a4a912
JPalacio3 <joelpalacio630@gmail.com>
2025-03-11 18:26:07 -0600

    Eliminar tipos innecesarios de ExpenseForm y agregar definiciones de tipos para Expense y DraffExpense

---
+++ src/types/index.ts
@@ -0,0 +1,18 @@
+export type Expense = {
+  id: string;
+  expenseName: string;
+  amount: number;
+  category: string;
+  date: Value;
+}
+
+export type DraffExpense = {
+  expenseName: string;
+  amount: number;
+  category: string;
+  date: Value;
+}
+
+
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
