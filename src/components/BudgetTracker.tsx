79ff5209b4259bdb6fbe0b07f66ec62c86afa1d6
JPalacio3 <joelpalacio630@gmail.com>
2025-04-17 15:10:19 -0600

    Refactor BudgetTracker component to calculate total and remaining expenses using useMemo

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,24 +1,35 @@
+import { useMemo } from "react";
+import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
-	return (
-		<div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-			<div className="flex justify-center">
-				<img src="/grafico.jpg" alt="Grafico de gastos" />
-			</div>
+  const { state } = useBudget();
 
-			<div className="flex flex-col justify-center items-center gap-8">
-				<button
-					className="bg-pink-600 hover:bg-pink-700 w-full md:w-auto text-white
+  const totalExpenses = useMemo(
+    () => state.expenses.reduce((total, expense) => expense.amount + total, 0),
+    [state.expenses]
+  );
+
+  const remainingExpenses = state.budget - totalExpenses;
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+      <div className="flex justify-center">
+        <img src="/grafico.jpg" alt="Grafico de gastos" />
+      </div>
+
+      <div className="flex flex-col justify-center items-center gap-8">
+        <button
+          className="bg-pink-600 hover:bg-pink-700 w-full md:w-auto text-white
 				font-bold rounded-lg p-2 uppercase"
-				>
-					Resetear App
-				</button>
+        >
+          Resetear App
+        </button>
 
-				<AmountDisplay label="Presupuesto" amount={500} />
-				<AmountDisplay label="Disponible" amount={300} />
-				<AmountDisplay label="Gastado" amount={200} />
-			</div>
-		</div>
-	);
+        <AmountDisplay label="Presupuesto" amount={state.budget} />
+        <AmountDisplay label="Disponible" amount={remainingExpenses} />
+        <AmountDisplay label="Gastado" amount={totalExpenses} />
+      </div>
+    </div>
+  );
 }
